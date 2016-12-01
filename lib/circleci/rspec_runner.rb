ee3dfef1bb7bdfc9385db3d14ca9a01fa6196e9d
Zach Taylor <taylorzr@gmail.com>
2016-12-01 17:33:36 -0600

    Gem template

---
+++ lib/circleci/rspec_runner.rb
@@ -0,0 +1,7 @@
+require 'circleci/rspec_runner/version'
+
+module Circleci
+  module RspecRunner
+    # Your code goes here...
+  end
+end
---
+++ lib/circleci/rspec_runner/version.rb
@@ -0,0 +1,5 @@
+module Circleci
+  module RspecRunner
+    VERSION = '0.1.0'
+  end
+end
